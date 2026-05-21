9ebec6e58b4642bbd5a4c80818524d07daccbd81
DESKTOP-OBTD4P4\Admin <huynhquochieu127@gmail.com>
2026-05-21 15:25:05 +0700

    Initial commit

---
+++ coffee-shop-backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Route test cơ bản
+app.get("/", (req, res) => {
+  res.send("Coffee Shop Backend đang chạy!");
+});
+
+// Khởi chạy server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server đang chạy tại http://localhost:${PORT}`);
+});
